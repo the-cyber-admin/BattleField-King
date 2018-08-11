e497e08fb15efbbc6cef4a74cf73431017f8fc63
the-cyber-admin <38099115+the-cyber-admin@users.noreply.github.com>
2018-08-11 10:02:20 +0300

    Move , Shoot
    Add Movement (PlayerMotor)
    Add Gun Controller (GunRotaor)

---
+++ Assets/Scripts/GunRotator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class GunRotator : MonoBehaviour
+{
+
+	public Transform center;
+	public int invert = 1;
+	
+	Camera cam;
+	void Awake()
+	{
+		cam = Camera.main;
+	}
+	
+	void Update () 
+	{
+		//Get the Screen positions of the object
+		Vector2 positionOnScreen = cam.WorldToViewportPoint (center.position);
+         
+		//Get the Screen position of the mouse
+		Vector2 mouseOnScreen = cam.ScreenToViewportPoint(Input.mousePosition);
+         
+		//Get the angle between the points
+		float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+ 
+		//Assign the new Rotation
+		center.rotation =  Quaternion.Euler (new Vector3(0f,angle,0f));
+	}
+ 
+	float AngleBetweenTwoPoints(Vector3 b, Vector3 a)
+	{
+		a *= invert;
+		b *= invert;
+		
+		return Mathf.Atan2(a.x - b.x, a.y - b.y) * Mathf.Rad2Deg;
+	}
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+	public float speed = 5f;
+	public float movementSmoothing = 0.05f;
+
+	Vector2 axis;
+	
+	Rigidbody rb;
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+	{
+		axis.x = Input.GetAxis("Horizontal");
+		axis.y = Input.GetAxis("Vertical");
+	}
+
+	Vector3 velocity;
+	void FixedUpdate()
+	{
+		var axisReal = new Vector3(axis.x, 0, axis.y);
+		//rb.AddForce(axis * speed * Time.fixedDeltaTime);
+		var force = axisReal * speed;
+		Vector3 targetVelocity = new Vector3(force.x , 0 , force.z);
+		rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, movementSmoothing);
+	}
+}
