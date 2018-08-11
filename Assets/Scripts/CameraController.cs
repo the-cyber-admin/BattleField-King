d09d8454b93ee7ab133be82ef462ae9c7c600af6
the-cyber-admin <38099115+the-cyber-admin@users.noreply.github.com>
2018-08-11 10:34:43 +0300

    Realtime
    Add Shrink Component to th Battlefield
    Add Camera Controller to follow the Player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+	public Transform target;
+	public float speed = 3f;
+	public float smoothTime = 0.5f;
+	public Vector3 offset;
+	
+	Vector3 velocity;
+	void FixedUpdate ()
+	{
+		var newPosition = 
+			new Vector3(target.position.x, transform.position.y, target.position.z) + offset;
+		transform.position = 
+			Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+	}
+}
---
+++ Assets/Scripts/Shrink.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Shrink : MonoBehaviour
+{
+	[HideInInspector]
+	public float persentage = 1f;
+
+	public float speed = 1f;
+
+	void Update()
+	{
+		persentage -= speed * Time.deltaTime;
+		var scale = new Vector3(persentage, transform.localScale.y, persentage);
+		
+		scale.x = Mathf.Clamp(scale.x, 0f, 1f);
+		scale.z = Mathf.Clamp(scale.z, 0f, 1f);
+		
+		transform.localScale = scale;
+
+
+	}
+}
